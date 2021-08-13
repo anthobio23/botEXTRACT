1cf2397538eb954a5a0a9eaa6848f67f2f3d447c
perceptron <perceptron-v1@localhost.localdomain>
2021-08-13 02:37:25 -0500

    version 2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import time
 import random as rd
 import pandas as pd
+
 from selenium.webdriver.firefox import options
 import numpy as np
 from selenium import webdriver
@@ -10,36 +11,50 @@ from selenium.webdriver.firefox.options import Options
 
 PATH = "https://www.intime.cl/sostenes?PS=16&O=OrderByReleaseDateDESC"
 driver_options = Options()
-driver_options.add_argument('--headless')
-driver_options.add_argument('--no-sandbox')
+#driver_options.add_argument('--headless')
+#driver_options.add_argument('--no-sandbox')
 driver = webdriver.Firefox(executable_path = './geckodriver', 
                            options = driver_options)
 driver.get(PATH)
-assert len(driver.window_handles) == 1
+time.sleep(rd.randint(5.0, 8.0))
+#assert len(driver.window_handles) == 1
 
-def lector_file(l_COD):
+class DOM:
+
+    def lector_file():
     
-    l_COD = []
-    with open("COD.txt", "r") as file:
-        for line in file:
-            l_COD.append(line)
-            pass
-        pass
-    return l_COD
+        with open("COD.txt", "r") as file:
+            l_COD = [line.rstrip() for line in file]
+        return l_COD
 
-def slow_typing(element, text):
-    for character in text:
-        element.send_keys(character)
-        time.sleep(rd.randint(2.0, 8.0))
+    def extract_elemt():
+
+        box_items = driver.find_elements_by_xpath('//div[@class="section__prateleira-product"]')
+        for iter in box_items:
+            items = iter.find_element_by_class_name('product-image')
+            items.click()
+            pass
+
+        box_prod_it = driver.find_elements_by_xpath('//div[@class="product-content__sheet-right"]')
+        for i in box_prod_it:
+
+            title = i.find_elements_by_xpath('//div[@class="product-content__sheet-right--name"]')
+
+            print(title[0].text) # funciona, recoge el texto plano del DOM dentro de un div
+
+#            desc = i.find_element_by_xpath('//div[@class="product-content__sheet-right--description"]/div').text()
+#            print(desc)
+          
         pass
     pass
 
 def search():
-    buscador = driver.find_element_by_class_name("fulltext-search-box ui-autocomplete-input")
-    slow_typing(buscador, "76895")
+
+    buscador = driver.find_element_by_xpath('//input[@class="fulltext-search-box ui-autocomplete-input"]')
+    cod = DOM.lector_file()
+    buscador.send_keys(str(cod[0]) + Keys.ENTER)
+    time.sleep(rd.randint(2.0, 8.0))
+    DOM.extract_elemt()
     pass
 
-
-
-
-
+search()
---
+++ items.py
@@ -0,0 +1,20 @@
+from _typeshed import Self
+from typing import Sized
+
+
+class ProductItems:
+    """ objeto incial de recogida de datos para publicaciones
+    :methods __init__
+    
+    parameters: ordern de argumentos
+    """
+    
+    def __init__(self, **kwargs) -> None:
+        
+        self.title = str(title)
+        self.size = str(size)
+        self.sku = int(sku)
+        self.desc = str(desc)
+        self.comp = str(comp)
+        
+        pass
